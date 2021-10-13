13e87dea76bff1ee41bbc50a498aacee3779625b
dnslin <bolekmiss@gmail.com>
2021-10-13 23:08:58 +0800

    更新请求列表

---
+++ src/main/java/com/dnslin/dnslin_api/controller/api/ApiRequestList.java
@@ -0,0 +1,21 @@
+package com.dnslin.dnslin_api.controller.api;
+
+import com.dnslin.dnslin_api.result.R;
+import com.dnslin.dnslin_api.result.ResponseEnum;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin
+
+public class ApiRequestList {
+    @GetMapping("/api")
+    public R getRequestList(){
+        String [] list = {"/api/cdkey","/api/edns","/api/icp","/api/IPPosition","/api/ipquerys","By:林祈 https://github.com/dnslin/Dnslin_Api"};
+        return new R(ResponseEnum.SUCCESS,list);
+    }
+}
