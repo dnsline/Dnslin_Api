fac87d93063db99c66dd856b413ba3f0d8b91e28
dnslin <bolekmiss@gmail.com>
2021-10-14 15:57:31 +0800

    增加国内api定位查询 （针对某些IDC IP地址比较准确）

--- src/main/java/com/dnslin/dnslin_api/controller/api/AmapIPController.java
+++ src/main/java/com/dnslin/dnslin_api/controller/api/AmapIPController.java
@@ -1,4 +1,30 @@
 package com.dnslin.dnslin_api.controller.api;
 
+import com.dnslin.dnslin_api.entity.AmapIP;
+import com.dnslin.dnslin_api.result.R;
+import com.dnslin.dnslin_api.result.ResponseEnum;
+import com.dnslin.dnslin_api.service.AmapIpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class AmapIPController {
+    @Autowired
+    private AmapIpService amapIpService;
+    @GetMapping("/amap")
+    public R ToAutonaviLocation(String ipaddress){
+        if (ipaddress==null || ipaddress.isEmpty()){
+            return new R(ResponseEnum.PARAMETERS_ARE_MISSING,null);
+        }
+        AmapIP amapIP = amapIpService.makeASignature(ipaddress);
+        if ("1".equals(amapIP.getStatus()) == false) {
+            return new R(ResponseEnum.SUCCESS,amapIP.getInfo());
+        }
+        return new R(ResponseEnum.SUCCESS,amapIP);
+    }
 }
--- src/main/java/com/dnslin/dnslin_api/controller/api/ApiRequestList.java
+++ src/main/java/com/dnslin/dnslin_api/controller/api/ApiRequestList.java
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-
 public class ApiRequestList {
     @GetMapping("/api")
     public R getRequestList(){
-        String [] list = {"/api/cdkey","/api/edns","/api/icp","/api/IPPosition","/api/ipquerys","By:林祈 https://github.com/dnslin/Dnslin_Api"};
+        String [] list = {"/api/cdkey","/api/edns","/api/icp","/api/IPPosition","/api/ipquerys","/api/amap","By:林祈 https://github.com/dnslin/Dnslin_Api"};
         return new R(ResponseEnum.SUCCESS,list);
     }
 }
